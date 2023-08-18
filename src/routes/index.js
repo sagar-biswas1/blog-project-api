4cbe44e9ef0ed73f9081b7cedfda9abbaeb81ea3
sagar-biswas1 <bsagor99@gmail.com>
2023-08-18 14:29:33 +0600

    updateItemPatch for v2 setup done

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const { controllers : articlesControllers} =require('../api/v1/article')
+const { controllers : articlesControllersV2} =require('../api/v2/article')
 
 router
 .route("/api/v1/articles")
@@ -12,5 +13,13 @@ router
 .get(articlesControllers.findByID)
 .patch(articlesControllers.updateItemPatch)
 .delete(articlesControllers.removeItem)
+
+
+router
+.route("/api/v2/articles/:id")
+// .put(articlesControllers.updateItem)
+// .get(articlesControllers.findByID)
+.patch(articlesControllersV2.updateItemPatch)
+// .delete(articlesControllers.removeItem)
 
 module.exports = router
